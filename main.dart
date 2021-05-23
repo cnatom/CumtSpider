f75625ab660ee76b0da595956873d16da579bd77
cnatom <1004275481@qq.com>
2021-05-23 13:23:23 +0800

    用于爬取矿大信息网站

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'cumt.dart';
+
+main()async{
+  Cumt cumt = new Cumt('08192988','Redsunjinyi');
+  // await cumt.login(); //登录教务
+  // await cumt.inquiry(InquiryType.ScoreAll, '2019', '12'); //获取成绩-无明细有补考
+  // await cumt.inquiry(InquiryType.Score, '2019', '12');//获取成绩-有明细无补考
+  await cumt.inquiry(InquiryType.Course, '2020', '12');//获取课表
+  // await cumt.inquiry(InquiryType.Exam, '2019', '12');//获取考试信息
+  // await cumt.initVideo(); //获取训课系统cookie
+  // await cumt.searchVideo(courseName:'大数据导论'); //查询训课系统视频
+}
